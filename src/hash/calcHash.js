efdfdecb547a35c320e8afa22883a07dfee938fc
Natallia Fedartsova <fedorcova@ktc.rw>
2024-10-02 14:48:52 +0300

    feat: completed hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+
+function getSha256Hash(path) {
+    return new Promise((resolve, reject) => {
+        const hash = crypto.createHash('sha256');
+        const readStream = fs.createReadStream(path);
+        readStream.on('error', reject);
+        readStream.on('data', chunk => hash.update(chunk));
+        readStream.on('end', () => resolve(hash.digest('hex')));
+    });
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const hashValue = await getSha256Hash(path.join(import.meta.dirname, 'files/fileToCalculateHashFor.txt'));
+        process.stdout.write(`${hashValue}\n`);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 };
 
 await calculateHash();
