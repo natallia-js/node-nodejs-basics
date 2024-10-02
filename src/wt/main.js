8970795618fb658beff6f523cf3c45937914a586
Natallia Fedartsova <fedorcova@ktc.rw>
2024-10-02 16:51:04 +0300

    feat: completed worker threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import path from 'path';
+import os from 'os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const systemCpuCoresNumber = os.cpus().length;
+    let responsesReceived = 0;
+    const workersArray = [];
+    const outputResults = () => console.log(workersArray.map((el) => ({ status: el.status, data: el.data })));
+    const checkForEnd = () => {
+        if (responsesReceived === systemCpuCoresNumber)
+            outputResults();
+    };
+    for (let i = 0, numberToStartWith = 10; i < systemCpuCoresNumber; i++, numberToStartWith++) {
+        workersArray.push({ workerId: numberToStartWith });
+        const worker = new Worker(path.join(import.meta.dirname, 'worker.js'));
+        worker
+        .on('online', () => {
+            worker.postMessage(numberToStartWith);
+        })
+        .on('message', ({ result }) => {
+            const workerArrayElement = workersArray.find(el => el.workerId === numberToStartWith);
+            workerArrayElement.status = 'resolved';
+            workerArrayElement.data = result;
+            responsesReceived++;
+            worker.unref();
+            checkForEnd();
+        })
+        .on('error', () => {
+            const workerArrayElement = workersArray.find(el => el.workerId === numberToStartWith);
+            workerArrayElement.status = 'error';
+            workerArrayElement.data = null;
+            responsesReceived++;
+            worker.unref();
+            checkForEnd();
+        });
+    }
 };
 
 await performCalculations();
