27059c8a551d8ee8fcf2096c99ffeed30ad36edb
Natallia Fedartsova <fedorcova@ktc.rw>
2024-10-03 13:50:48 +0300

    feat: completed child processes subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import path from 'path';
+import child_process from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = child_process.fork(
+        path.join(import.meta.dirname, 'files/script.js'),
+        args,
+        { stdio: [process.stdin, process.stdout, process.stderr, 'ipc'] });
+    childProcess.on('error', (err) => {
+        console.log(`Failed to start child process: ${err}`);
+        process.exit(1);
+    });
+    childProcess.on('close', (code) => {
+        console.log(`Child process exited. Code: ${code}`);
+        process.exit(0);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([ 1, 2, 'Hello, world!' ]);
